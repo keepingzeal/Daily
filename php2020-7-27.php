127a8e9b064a3392cfb936c52cf1661c098df9c2
kzeal <keepingzeal@163.com>
2020-07-27 18:08:01 +0800

    微调

---
+++ php2020-7-27.php
@@ -0,0 +1,59 @@
+<?php
+class Item
+{
+	public $email;
+	public $name;
+
+	public function setEmail($val) {
+		$this->email = $val;
+	}
+
+	public function setName($val) {
+		$this->name = $val;
+	}
+}
+
+
+class Collection
+{
+	protected $items;
+
+	public function __construct($items) {
+		$this->items = $items;
+	}
+
+	public function map($callback) {
+		return new static(array_map($callback, $this->items));
+	}
+
+	public function filter($callback) {
+		return new static(array_filter($this->items, $callback));
+	}
+
+	public function toArray() {
+		return $this->items;
+	}
+}
+
+$Item = array();
+$Item[0] = new Item();
+$Item[1] = new Item();
+$Item[2] = new Item();
+
+$Item[0]->setEmail(1111);
+$Item[1]->setEmail(2222);
+$Item[1]->setName(2222);
+$Item[2]->setEmail(3333);
+$Item[2]->setName(3333);
+
+
+
+function getEmail($items) {
+	return array_map(function($item){
+		return $item->email;
+	}, array_filter($items, function($item){
+		return $item->email != null;
+	}));
+}
+
+var_dump(getEmail($Item));
